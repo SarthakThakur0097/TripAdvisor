66bde7603516d55636a626526f499d4eeacfae0c
Sarthak Thakur <50295928+SarthakThakur0097@users.noreply.github.com>
2020-10-08 19:17:15 -0400

    Implemented all API endpoints

--- TripAdvisor_React/TripAdvisor_React/Data/Models/Services/TripService.cs
+++ TripAdvisor_React/TripAdvisor_React/Data/Models/Services/TripService.cs
@@ -12,19 +12,30 @@ namespace TripAdvisor_React.Data.Models.Services
 
         public void DeleteTrip(int tripId)
         {
-            throw new System.NotImplementedException();
+            var trip = Data.Trips.FirstOrDefault(n => n.Id == tripId);
+
+            if(trip != null)
+            {
+                Data.Trips.Remove(trip);
+            }
         }
 
         public List<Trip> GetAllTrips() => Data.Trips.ToList();
         
-        public Trip GetTripById(int tripId)
-        {
-            throw new System.NotImplementedException();
-        }
+        public Trip GetTripById(int tripId) => Data.Trips.FirstOrDefault(n => n.Id == tripId);
 
         public void UpdateTrip(int tripId, Trip trip)
         {
-            throw new System.NotImplementedException();
+            var oldTrip = Data.Trips.FirstOrDefault(n => n.Id == tripId);
+
+            if(oldTrip != null)
+            {
+                oldTrip.Name = trip.Name;
+                oldTrip.Description = trip.Description;
+                oldTrip.DateStarted = trip.DateStarted;
+                oldTrip.DateCompleted = trip.DateCompleted;
+
+            }
         }
     }
 }
